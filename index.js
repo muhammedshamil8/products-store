dce764b0948d5260fa2f094b13f08af581104344
Shamil <muhammedshamil008@gmail.com>
2023-09-11 00:45:05 +0530

    #

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-let todoList = JSON.parse(localStorage.getItem('todoList')) || [
-     {
-
-     }
-];
+let todoList = JSON.parse(localStorage.getItem('todoList')) || [{}];
 
 
 function todoListrender() {
