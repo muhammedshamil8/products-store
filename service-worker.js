0ae870b369c89e797b021316fbb6784523fc6841
Shamil <muhammedshamil008@gmail.com>
2023-09-11 00:40:12 +0530

    adding web app option

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,41 @@
-// service-worker.js
-self.addEventListener('install', (event) => {
-     event.waitUntil(
-       caches.open('my-cache').then((cache) => {
-         // Cache multiple files and assets here
-         return cache.addAll([
-           './index.html',
-           './styles.css', // Add paths to your CSS files
-           './script.js', // Add paths to your JavaScript files
-           './icon.png', // Add paths to your images
-           // Add more files and assets as needed
-         ]);
-       })
-     );
-   });
-   
-   self.addEventListener('fetch', (event) => {
-     event.respondWith(
-       caches.match(event.request).then((response) => {
-         // Serve cached files if available, otherwise fetch from the network
-         return response || fetch(event.request);
-       })
-     );
-   });
-   
+const CACHE_NAME = 'todo-app-cache-v1';
+const urlsToCache = [
+  '/',
+  'styles.css',
+  'index.js',
+  'index.js',
+  'birdlogo7.jpg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        
+        // If the requested resource is not in cache, fetch it
+        return fetch(event.request)
+          .then(fetchResponse => {
+            // Clone the response to store in the cache
+            const clone = fetchResponse.clone();
+            
+            // Cache the fetched resource
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, clone);
+              });
+            
+            return fetchResponse;
+          });
+      })
+  );
+});
