7cda8f7e5f1b9da24ae7b0096bcd6a7600616523
strikerlulu <strikerlulu7@gmail.com>
2023-11-02 13:10:01 +0530

    #

--- service-worker.js
+++ service-worker.js
@@ -7,33 +7,44 @@ const urlsToCache = [
   'icon.png',
   'offline.html' 
 ];
-
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then((response) => {
+      .then(response => {
         if (response) {
-          return response; // Serve cached assets
+          return response;
         }
 
-        // If the request is not in the cache, fetch it from the network
+        // If the requested resource is not in cache, serve the offline page
         return fetch(event.request)
-          .then((networkResponse) => {
-            // Cache the API response
-            if (event.request.url.startsWith('/api/')) {
-              caches.open(CACHE_NAME)
-                .then((cache) => {
-                  cache.put(event.request, networkResponse.clone());
-                });
+          .catch(() => caches.match('offline.html')) // Serve offline.html when there's an error
+          .then(notFoundResponse => {
+            if (notFoundResponse) {
+              return notFoundResponse;
             }
-
-            return networkResponse;
-          })
-          .catch(() => {
-            // If both cache and network fail, serve offline page
-            return caches.match('offline.html');
+            // If offline.html is also not found in the cache, you can return a simple response
+            // with a custom message.
+            
+            return fetch(event.request)
+            .then((networkResponse) => {
+              // Cache the API response
+              if (event.request.url.startsWith('/api/')) {
+                caches.open(CACHE_NAME)
+                  .then((cache) => {
+                    cache.put(event.request, networkResponse.clone());
+                  });
+              }
+  
+              return networkResponse;
+            })
+            .catch(() => {
+              // If both cache and network fail, serve offline page
+              return caches.match('offline.html');
+            });
+  
           });
       })
   );
 });
+
 
