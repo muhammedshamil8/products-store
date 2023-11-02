0b6fbf5097eb5c6bfcbb1b8134a3cee233e979bc
strikerlulu <strikerlulu7@gmail.com>
2023-11-02 17:35:59 +0530

    #

--- service-worker.js
+++ service-worker.js
@@ -7,14 +7,14 @@ const urlsToCache = [
      'offline.html',
      'icon.png',
    ];
-   self.addEventListener('install', (event) => {
-     event.waitUntil(
-       caches.open(CACHE_NAME)
-         .then((cache) => {
-           return cache.addAll(urlsToCache);
-         })
-     );
-   });
+//    self.addEventListener('install', (event) => {
+//      event.waitUntil(
+//        caches.open(CACHE_NAME)
+//          .then((cache) => {
+//            return cache.addAll(urlsToCache);
+//          })
+//      );
+//    });
    self.addEventListener('fetch', event => {
      event.respondWith(
        caches.match(event.request)
