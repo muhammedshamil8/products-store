ec89c4bb9d26a9d11a9a1219d9cf84e22b036d58
strikerlulu <strikerlulu7@gmail.com>
2023-11-02 16:36:48 +0530

    final...stoped

--- service-worker.js
+++ service-worker.js
@@ -15,55 +15,31 @@ const urlsToCache = [
          })
      );
    });
-   self.addEventListener('fetch', (event) => {
+   self.addEventListener('fetch', event => {
      event.respondWith(
-       Promise.race([
-         fetch(event.request)
-           .then((response) => {
-             // Cache the fetched response for future use
-             const responseClone = response.clone();
-             caches.open(CACHE_NAME)
-               .then((cache) => cache.put(event.request, responseClone));
+       caches.match(event.request)
+         .then(response => {
+           if (response) {
              return response;
-           })
-           .catch(() => {
-             // Network request failed, try to serve an offline page
-             return caches.match('offline.html');
-           }),
-         new Promise((_, reject) => {
-           // Set a timeout for the network request
-           setTimeout(() => reject(new Error('Request timed out')), 5000); // Adjust the timeout as needed
+           }
+   
+           // If the requested resource is not in cache, serve the offline page
+           return fetch(event.request)
+             .catch(() => caches.match('offline.html')) // Serve offline.html when there's an error
+             .then(notFoundResponse => {
+               if (notFoundResponse) {
+                 return notFoundResponse;
+               }
+               // If offline.html is also not found in the cache, you can return a simple response
+               // with a custom message.
+               return new Response('<div style="margin:auto"></div><p>Something fishy...<br />dont reload in offline mode just use it!</p></div>'
+              
+               , 
+               {
+                 headers: { 'Content-Type': 'text/html' }
+               });
+             });
          })
-       ])
      );
    });
-   
-
-//    self.addEventListener('fetch', event => {
-//      event.respondWith(
-//        caches.match(event.request)
-//          .then(response => {
-//            if (response) {
-//              return response;
-//            }
-   
-//            // If the requested resource is not in cache, serve the offline page
-//            return fetch(event.request)
-//              .catch(() => caches.match('offline.html')) // Serve offline.html when there's an error
-//              .then(notFoundResponse => {
-//                if (notFoundResponse) {
-//                  return notFoundResponse;
-//                }
-//                // If offline.html is also not found in the cache, you can return a simple response
-//                // with a custom message.
-//                return new Response('index.html'
-              
-//                , 
-//                {
-//                  headers: { 'Content-Type': 'html/javascript' }
-//                });
-//              });
-//          })
-//      );
-//    });
    
