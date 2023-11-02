a23f448c548e2043d275fecc7d99708516b1b6f2
strikerlulu <strikerlulu7@gmail.com>
2023-11-02 13:05:02 +0530

    offline page updated

--- service-worker.js
+++ service-worker.js
@@ -7,27 +7,31 @@ const urlsToCache = [
   'icon.png',
   'offline.html' 
 ];
-self.addEventListener('fetch', event => {
+
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
+      .then((response) => {
         if (response) {
-          return response;
+          return response; // Serve cached assets
         }
 
-        // If the requested resource is not in cache, serve the offline page
+        // If the request is not in the cache, fetch it from the network
         return fetch(event.request)
-          .catch(() => caches.match('offline.html')) // Serve offline.html when there's an error
-          .then(notFoundResponse => {
-            if (notFoundResponse) {
-              return notFoundResponse;
+          .then((networkResponse) => {
+            // Cache the API response
+            if (event.request.url.startsWith('/api/')) {
+              caches.open(CACHE_NAME)
+                .then((cache) => {
+                  cache.put(event.request, networkResponse.clone());
+                });
             }
-            // If offline.html is also not found in the cache, you can return a simple response
-            // with a custom message.
-            
-            // return new Response('<h1>Offline</h1><p>You are currently offline.</p>', {
-            //   headers: { 'Content-Type': 'text/html' }
-            // });
+
+            return networkResponse;
+          })
+          .catch(() => {
+            // If both cache and network fail, serve offline page
+            return caches.match('offline.html');
           });
       })
   );
