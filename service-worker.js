e92935d9fdea946f31f2050f3dead5f8813f2b5b
strikerlulu <strikerlulu7@gmail.com>
2023-11-02 16:10:32 +0530

    updated

--- service-worker.js
+++ service-worker.js
@@ -4,8 +4,8 @@ const urlsToCache = [
      'styles.css',
      'index.html',
      'index.js',
+     'offline.html',
      'icon.png',
-     'offline.html' 
    ];
    self.addEventListener('install', (event) => {
      event.waitUntil(
@@ -17,29 +17,45 @@ const urlsToCache = [
    });
    self.addEventListener('fetch', event => {
      event.respondWith(
-       caches.match(event.request)
+       fetch(event.request)
+         .catch(() => caches.match(event.request))
          .then(response => {
            if (response) {
              return response;
            }
-   
-           // If the requested resource is not in cache, serve the offline page
-           return fetch(event.request)
-             .catch(() => caches.match('offline.html')) // Serve offline.html when there's an error
-             .then(notFoundResponse => {
-               if (notFoundResponse) {
-                 return notFoundResponse;
-               }
-               // If offline.html is also not found in the cache, you can return a simple response
-               // with a custom message.
-               return new Response('index.html'
-              
-               , 
-               {
-                 headers: { 'Content-Type': 'html' }
-               });
-             });
+           // If the requested resource is not in cache and there's no network, serve the offline page
+           if (event.request.mode === 'navigate') {
+             return caches.match('offline.html');
+           }
          })
      );
    });
+
+//    self.addEventListener('fetch', event => {
+//      event.respondWith(
+//        caches.match(event.request)
+//          .then(response => {
+//            if (response) {
+//              return response;
+//            }
+   
+//            // If the requested resource is not in cache, serve the offline page
+//            return fetch(event.request)
+//              .catch(() => caches.match('offline.html')) // Serve offline.html when there's an error
+//              .then(notFoundResponse => {
+//                if (notFoundResponse) {
+//                  return notFoundResponse;
+//                }
+//                // If offline.html is also not found in the cache, you can return a simple response
+//                // with a custom message.
+//                return new Response('index.html'
+              
+//                , 
+//                {
+//                  headers: { 'Content-Type': 'html/javascript' }
+//                });
+//              });
+//          })
+//      );
+//    });
    
